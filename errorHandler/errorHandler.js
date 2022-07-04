6efef435fb504a321cbc314e17c5a7438d746044
Manita Gupta <mannie.gupta@gmail.com>
2022-07-04 12:41:11 +0100

    add /api/topics endPoint, that reponds with array of topic objects

---
+++ errorHandler/errorHandler.js
@@ -0,0 +1,18 @@
+exports.psqlErrorHandler = (err, req, res, next) => {
+  console.log(err);
+  if (err?.code) {
+    res.status(400).send({ msg: "Bad Request" });
+  }
+  next(err);
+};
+
+exports.handleCustomErrors = (err, req, res, next) => {
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  }
+  next(err);
+};
+
+exports.unhandledErrors = (err, req, res, next) => {
+  res.status(500).send({ msg: "server error" });
+};
