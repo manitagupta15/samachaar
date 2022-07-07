6dc219616903bc6c78650b1d2553c82ef6c17fd1
Manita Gupta <mannie.gupta@gmail.com>
2022-07-07 12:04:52 +0100

    remove comments from app.js

--- app.js
+++ app.js
@@ -4,44 +4,13 @@ const app = express();
 app.use(express.json());
 
 const apiRouter = require("./routes/api-router");
-/*
-const {
-  getTopics,
-  getArticlesByArticleId,
-  getUsers,
-  patchArticleByArticleId,
-  getCommentsByArticleId,
-  getArticles,
-  postComment,
-  getAllapi,
 
-  deleteCommentByCommentId,
-} = require("./controller/app.controller");
-*/
 const {
   psqlErrorHandler,
   handleCustomErrors,
   unhandledErrors,
   invalidPathError,
 } = require("./errorHandler/errorHandler");
-/*
-app.get("/api/topics", getTopics);
-
-app.get("/api/articles/:article_id", getArticlesByArticleId);
-
-app.patch("/api/articles/:article_id", patchArticleByArticleId);
-
-app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
-
-app.get("/api/articles", getArticles);
-
-app.get("/api/users", getUsers);
-
-app.post("/api/articles/:article_id/comments", postComment);
-
-app.get("/api", getAllapi);
-app.delete("/api/comments/:comment_id", deleteCommentByCommentId);
-*/
 
 app.use("/api", apiRouter);
 
